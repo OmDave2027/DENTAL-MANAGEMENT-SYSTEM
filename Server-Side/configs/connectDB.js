d716b5c92bd6f5ef4dc850cb12a4071d6e8e602e
OmDave2027 <omdave.2027@gmail.com>
2026-09-01 18:43:41 +0530

    Add MySQL connection pool to connectDB.js

--- Server-Side/configs/connectDB.js
+++ Server-Side/configs/connectDB.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connectDB = async () =>{
     try{
@@ -9,5 +13,16 @@ const connectDB = async () =>{
         process.exit(1);
     }
 };
+
+export const mysqlPool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
 export default connectDB;
