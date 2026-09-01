fd24ab78a99413a7f0a885ed04453e19204b7ac2
OmDave2027 <omdave.2027@gmail.com>
2026-09-01 19:22:15 +0530

    Remove unnecessary blank line in appointmentRoute.js

--- Server-Side/routes/appointmentRoute.js
+++ Server-Side/routes/appointmentRoute.js
@@ -2,7 +2,6 @@ import express from "express";
 import { body, validationResult } from "express-validator";
 import { mysqlPool } from "../configs/connectDB.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
-
 const router = express.Router();
 
 // GET all appointments for logged-in user
